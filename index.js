796affc8d05f70bcf05df6f9b8d987960f0b9ef0
MyoMyatAung <myatmyo299@gmail.com>
2024-08-26 12:54:21 +0700

    DONE

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require('express');
+const cors = require("cors");
 const connect = require("./config/connect");
 const authRouter = require("./routes/auth.routes");
 const app = express();
@@ -7,8 +8,10 @@ const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 connect();
 app.use("/api/v1", authRouter);
+app.get("/api/v1/health-check", (req, res) => res.status(200).json({ message: "SERVER IS RUNNING" }))
 
 // Start Server
 app.listen(PORT, async () => {
